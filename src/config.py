981db7afdeaa1b968f37d693b4c964626fe22487
rey4L <reynardetwaroo4@gmail.com>
2024-07-23 19:48:32 -0400

    Fix config.json visibility issue for Linux

--- src/config.py
+++ src/config.py
@@ -2,8 +2,10 @@ import json
 import os
 import sys
 
+pwd = os.path.dirname(os.path.abspath(__file__)) # Adapted from https://stackoverflow.com/questions/21957131/python-not-finding-file-in-the-same-directory
+
 def load_config():
-    config_path = '..\src\config.json'
+    config_path = os.path.join(pwd, 'config.json')
     if not os.path.exists(config_path):
         print("Config file not found. Please create a 'config.json' file.")
         sys.exit(1)
